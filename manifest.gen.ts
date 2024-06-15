2e9e4fabe47fe6457ecc99a1f56ea487788d28b8
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2024-06-15 18:35:14 -0300

    feat: add htmxform

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -40,33 +40,34 @@ import * as $$$$$$24 from "./sections/Events/TotalEvents.tsx";
 import * as $$$$$$25 from "./sections/Footer.tsx";
 import * as $$$$$$26 from "./sections/Footer/Footer.tsx";
 import * as $$$$$$27 from "./sections/Gallery.tsx";
-import * as $$$$$$28 from "./sections/Header/Header.tsx";
-import * as $$$$$$29 from "./sections/Images/BannerGrid.tsx";
-import * as $$$$$$30 from "./sections/Images/Carousel.tsx";
-import * as $$$$$$31 from "./sections/Images/ImageGallery.tsx";
-import * as $$$$$$32 from "./sections/Images/ShoppableBanner.tsx";
-import * as $$$$$$33 from "./sections/ItWorks.tsx";
-import * as $$$$$$34 from "./sections/Links/LinkTree.tsx";
-import * as $$$$$$35 from "./sections/Links/Shortcuts.tsx";
-import * as $$$$$$36 from "./sections/LocalWeather.tsx";
-import * as $$$$$$37 from "./sections/Miscellaneous/CampaignTimer.tsx";
-import * as $$$$$$38 from "./sections/Miscellaneous/CookieConsent.tsx";
-import * as $$$$$$39 from "./sections/Miscellaneous/Slide.tsx";
-import * as $$$$$$40 from "./sections/Newsletter/Newsletter.tsx";
-import * as $$$$$$41 from "./sections/Product/ImageGalleryFrontBack.tsx";
-import * as $$$$$$42 from "./sections/Product/NotFound.tsx";
-import * as $$$$$$43 from "./sections/Product/NotFoundChallenge.tsx";
-import * as $$$$$$44 from "./sections/Product/PartialProductAd.tsx";
-import * as $$$$$$45 from "./sections/Product/ProductAd.tsx";
-import * as $$$$$$46 from "./sections/Product/ProductDetails.tsx";
-import * as $$$$$$47 from "./sections/Product/ProductShelf.tsx";
-import * as $$$$$$48 from "./sections/Product/ProductShelfTabbed.tsx";
-import * as $$$$$$49 from "./sections/Product/SearchResult.tsx";
-import * as $$$$$$50 from "./sections/Product/ShelfWithImage.tsx";
-import * as $$$$$$51 from "./sections/Product/Wishlist.tsx";
-import * as $$$$$$52 from "./sections/Social/InstagramPosts.tsx";
-import * as $$$$$$53 from "./sections/Social/WhatsApp.tsx";
-import * as $$$$$$54 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$29 from "./sections/Header/Header.tsx";
+import * as $$$$$$28 from "./sections/HTMXForm.tsx";
+import * as $$$$$$30 from "./sections/Images/BannerGrid.tsx";
+import * as $$$$$$31 from "./sections/Images/Carousel.tsx";
+import * as $$$$$$32 from "./sections/Images/ImageGallery.tsx";
+import * as $$$$$$33 from "./sections/Images/ShoppableBanner.tsx";
+import * as $$$$$$34 from "./sections/ItWorks.tsx";
+import * as $$$$$$35 from "./sections/Links/LinkTree.tsx";
+import * as $$$$$$36 from "./sections/Links/Shortcuts.tsx";
+import * as $$$$$$37 from "./sections/LocalWeather.tsx";
+import * as $$$$$$38 from "./sections/Miscellaneous/CampaignTimer.tsx";
+import * as $$$$$$39 from "./sections/Miscellaneous/CookieConsent.tsx";
+import * as $$$$$$40 from "./sections/Miscellaneous/Slide.tsx";
+import * as $$$$$$41 from "./sections/Newsletter/Newsletter.tsx";
+import * as $$$$$$42 from "./sections/Product/ImageGalleryFrontBack.tsx";
+import * as $$$$$$43 from "./sections/Product/NotFound.tsx";
+import * as $$$$$$44 from "./sections/Product/NotFoundChallenge.tsx";
+import * as $$$$$$45 from "./sections/Product/PartialProductAd.tsx";
+import * as $$$$$$46 from "./sections/Product/ProductAd.tsx";
+import * as $$$$$$47 from "./sections/Product/ProductDetails.tsx";
+import * as $$$$$$48 from "./sections/Product/ProductShelf.tsx";
+import * as $$$$$$49 from "./sections/Product/ProductShelfTabbed.tsx";
+import * as $$$$$$50 from "./sections/Product/SearchResult.tsx";
+import * as $$$$$$51 from "./sections/Product/ShelfWithImage.tsx";
+import * as $$$$$$52 from "./sections/Product/Wishlist.tsx";
+import * as $$$$$$53 from "./sections/Social/InstagramPosts.tsx";
+import * as $$$$$$54 from "./sections/Social/WhatsApp.tsx";
+import * as $$$$$$55 from "./sections/Theme/Theme.tsx";
 
 const manifest = {
   "loaders": {
@@ -106,33 +107,34 @@ const manifest = {
     "site/sections/Footer.tsx": $$$$$$25,
     "site/sections/Footer/Footer.tsx": $$$$$$26,
     "site/sections/Gallery.tsx": $$$$$$27,
-    "site/sections/Header/Header.tsx": $$$$$$28,
-    "site/sections/Images/BannerGrid.tsx": $$$$$$29,
-    "site/sections/Images/Carousel.tsx": $$$$$$30,
-    "site/sections/Images/ImageGallery.tsx": $$$$$$31,
-    "site/sections/Images/ShoppableBanner.tsx": $$$$$$32,
-    "site/sections/ItWorks.tsx": $$$$$$33,
-    "site/sections/Links/LinkTree.tsx": $$$$$$34,
-    "site/sections/Links/Shortcuts.tsx": $$$$$$35,
-    "site/sections/LocalWeather.tsx": $$$$$$36,
-    "site/sections/Miscellaneous/CampaignTimer.tsx": $$$$$$37,
-    "site/sections/Miscellaneous/CookieConsent.tsx": $$$$$$38,
-    "site/sections/Miscellaneous/Slide.tsx": $$$$$$39,
-    "site/sections/Newsletter/Newsletter.tsx": $$$$$$40,
-    "site/sections/Product/ImageGalleryFrontBack.tsx": $$$$$$41,
-    "site/sections/Product/NotFound.tsx": $$$$$$42,
-    "site/sections/Product/NotFoundChallenge.tsx": $$$$$$43,
-    "site/sections/Product/PartialProductAd.tsx": $$$$$$44,
-    "site/sections/Product/ProductAd.tsx": $$$$$$45,
-    "site/sections/Product/ProductDetails.tsx": $$$$$$46,
-    "site/sections/Product/ProductShelf.tsx": $$$$$$47,
-    "site/sections/Product/ProductShelfTabbed.tsx": $$$$$$48,
-    "site/sections/Product/SearchResult.tsx": $$$$$$49,
-    "site/sections/Product/ShelfWithImage.tsx": $$$$$$50,
-    "site/sections/Product/Wishlist.tsx": $$$$$$51,
-    "site/sections/Social/InstagramPosts.tsx": $$$$$$52,
-    "site/sections/Social/WhatsApp.tsx": $$$$$$53,
-    "site/sections/Theme/Theme.tsx": $$$$$$54,
+    "site/sections/Header/Header.tsx": $$$$$$29,
+    "site/sections/HTMXForm.tsx": $$$$$$28,
+    "site/sections/Images/BannerGrid.tsx": $$$$$$30,
+    "site/sections/Images/Carousel.tsx": $$$$$$31,
+    "site/sections/Images/ImageGallery.tsx": $$$$$$32,
+    "site/sections/Images/ShoppableBanner.tsx": $$$$$$33,
+    "site/sections/ItWorks.tsx": $$$$$$34,
+    "site/sections/Links/LinkTree.tsx": $$$$$$35,
+    "site/sections/Links/Shortcuts.tsx": $$$$$$36,
+    "site/sections/LocalWeather.tsx": $$$$$$37,
+    "site/sections/Miscellaneous/CampaignTimer.tsx": $$$$$$38,
+    "site/sections/Miscellaneous/CookieConsent.tsx": $$$$$$39,
+    "site/sections/Miscellaneous/Slide.tsx": $$$$$$40,
+    "site/sections/Newsletter/Newsletter.tsx": $$$$$$41,
+    "site/sections/Product/ImageGalleryFrontBack.tsx": $$$$$$42,
+    "site/sections/Product/NotFound.tsx": $$$$$$43,
+    "site/sections/Product/NotFoundChallenge.tsx": $$$$$$44,
+    "site/sections/Product/PartialProductAd.tsx": $$$$$$45,
+    "site/sections/Product/ProductAd.tsx": $$$$$$46,
+    "site/sections/Product/ProductDetails.tsx": $$$$$$47,
+    "site/sections/Product/ProductShelf.tsx": $$$$$$48,
+    "site/sections/Product/ProductShelfTabbed.tsx": $$$$$$49,
+    "site/sections/Product/SearchResult.tsx": $$$$$$50,
+    "site/sections/Product/ShelfWithImage.tsx": $$$$$$51,
+    "site/sections/Product/Wishlist.tsx": $$$$$$52,
+    "site/sections/Social/InstagramPosts.tsx": $$$$$$53,
+    "site/sections/Social/WhatsApp.tsx": $$$$$$54,
+    "site/sections/Theme/Theme.tsx": $$$$$$55,
   },
   "matchers": {
     "site/matchers/utm.ts": $$$$$$$0,
---
+++ sections/HTMXForm.tsx
@@ -0,0 +1,61 @@
+import { useSection } from "deco/hooks/useSection.ts";
+import { SectionProps } from "deco/mod.ts";
+
+export interface Props {
+  reminders?: string[];
+}
+
+export async function loader(props: Props, req: Request, _ctx: unknown) {
+  if (req.headers.get("content-type") !== "application/x-www-form-urlencoded") {
+    return props;
+  }
+
+  const form = await req.formData();
+  const reminder = form.get("reminder");
+
+  if (reminder) {
+    if (!props.reminders) {
+      props.reminders = [];
+    }
+
+    props.reminders.push(reminder.toString());
+  }
+
+  return props;
+}
+
+export default function HTMXForm(props: SectionProps<typeof loader>) {
+  return (
+    <div>
+      <form
+        class="flex"
+        hx-post={useSection<typeof HTMXForm>({ props })}
+        hx-trigger="submit"
+        hx-target="closest section"
+        hx-swap="outerHTML"
+        hx-indicator="#reminderSubmit"
+      >
+        <label class="mr-4">Reminder</label>
+        <div class="flex flex-col">
+          <textarea class="textarea textarea-bordered" name="reminder" />
+          <button
+            id="reminderSubmit"
+            type="submit"
+            class="btn btn-primary mt-2"
+          >
+            <span class="[.htmx-request_&]:hidden inline">
+              Add reminder
+            </span>
+            <span class="hidden loading loading-spinner loading-xs [.htmx-request_&]:inline " />
+          </button>
+        </div>
+      </form>
+      <div class="border rounded">
+        <h3>Current reminders</h3>
+        <ul>
+          {props.reminders?.map((reminder) => <li>{reminder}</li>)}
+        </ul>
+      </div>
+    </div>
+  );
+}
